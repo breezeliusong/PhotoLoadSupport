f394d75b7fe24d048a1d69b4fc6725d4ed9750d6
v-liuson <v-liuson@MSDNUWPSERVER1>
2017-03-02 15:35:23 +0800

    Add project files.

---
+++ PhotoLoadSupportC++/MainPage.xaml.cpp
@@ -0,0 +1,116 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace PhotoLoadSupportC__;
+
+using namespace Platform;
+using namespace concurrency;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Storage::Pickers;
+using namespace Windows::Storage;
+using namespace Windows::Storage::FileProperties;
+using namespace Windows::Storage::Search;
+using namespace Windows::Storage::BulkAccess;
+using namespace Platform::Collections;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+void LogMessage(Object^ parameter)
+{
+	auto paraString = parameter->ToString();
+	auto formattedText = std::wstring(paraString->Data()).append(L"\r\n");
+	OutputDebugString(formattedText.c_str());
+}
+
+void PhotoLoadSupportC__::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	FileOpenPicker^ openPicker = ref new FileOpenPicker();
+	openPicker->ViewMode = PickerViewMode::Thumbnail;
+	openPicker->SuggestedStartLocation = PickerLocationId::PicturesLibrary;
+	openPicker->FileTypeFilter->Append(".png");
+	create_task(openPicker->PickSingleFileAsync()).then([this](StorageFile^ file)
+	{
+		if (file)
+		{
+			//TO DO operate the file 
+
+			//loader other the same type  file
+			auto fileTypeFilter = ref new Vector<String^>();
+			fileTypeFilter->Append(".png");
+
+			// Create query options.
+			auto queryOptions = ref new QueryOptions(CommonFileQuery::OrderByName, fileTypeFilter);
+			create_task(KnownFolders::GetFolderForUserAsync(nullptr /* current user */, KnownFolderId::PicturesLibrary)).then([this, queryOptions](StorageFolder^ picturesFolder)
+			{
+				auto query = picturesFolder->CreateFileQueryWithOptions(queryOptions);
+				return query->GetFilesAsync();
+			}).then([=](IVectorView<StorageFile^>^ files)
+			{
+				std::for_each(begin(files), end(files), [=](StorageFile^ file)
+				{
+					// Create UI elements for the output and fill in the contents when they are available.
+					LogMessage(file->Name);
+				});
+			});
+		}
+		else
+		{
+			//fail to get file 
+		}
+	});
+}
+
+
+void PhotoLoadSupportC__::MainPage::Button_Click1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	FolderPicker^ folderPicker = ref new FolderPicker();
+	folderPicker->SuggestedStartLocation = PickerLocationId::PicturesLibrary;
+
+	// Users expect to have a filtered view of their folders depending on the scenario.
+	// For example, when choosing a documents folder, restrict the filetypes to documents for your application.
+	folderPicker->FileTypeFilter->Append("*");
+
+	create_task(folderPicker->PickSingleFolderAsync()).then([this](StorageFolder^ folder)
+	{
+		if (folder)
+		{
+			//loader other the same type  file
+			auto fileTypeFilter = ref new Vector<String^>();
+			fileTypeFilter->Append(".png");
+
+			// Create query options.
+			auto queryOptions = ref new QueryOptions(CommonFileQuery::OrderByName, fileTypeFilter);
+			auto query = folder->CreateFileQueryWithOptions(queryOptions);
+			create_task(query->GetFilesAsync()).then([this](IVectorView<StorageFile^>^ files) {
+
+				std::for_each(begin(files), end(files), [=](StorageFile^ file)
+				{
+					// Create UI elements for the output and fill in the contents when they are available.
+					LogMessage(file->Name);
+				});
+			});
+		}
+		else
+		{
+		}
+	});
+}
---
+++ PhotoLoadSupportCSharp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PhotoLoadSupportCSharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                StorageFolder folder= await file.GetParentAsync();
+                List<string> fileTypeFilter = new List<string>();
+                fileTypeFilter.Add(".png");
+                var queryOptions = new QueryOptions(CommonFileQuery.OrderByName, fileTypeFilter);
+
+                // Create query and retrieve files
+                var query = KnownFolders.PicturesLibrary.CreateFileQueryWithOptions(queryOptions);
+                IReadOnlyList<StorageFile> fileList = await query.GetFilesAsync();
+                // Process results
+                foreach (StorageFile item in fileList)
+                {
+                    // Process file
+                    System.Diagnostics.Debug.WriteLine(item.DisplayName);
+                }
+            }
+            else
+            {
+            }
+        }
+
+    }
+}
